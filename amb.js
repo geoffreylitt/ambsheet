72e1ce8a89bb4c8b619f50d95c4e4987ba34c352
Geoffrey Litt <gklitt@gmail.com>
2024-03-29 14:19:57 -0600

    support all arithmetic

--- amb.js
+++ amb.js
@@ -1,68 +1,45 @@
+function interpretBinaryOp(node, cont, op) {
+  interpret(node.arg1, (val1) => {
+    interpret(node.arg2, (val2) => {
+      cont(op(val1, val2));
+    });
+  });
+}
+
 function interpret(node, cont) {
   switch (node.type) {
     case "NumberNode":
       cont(node.value);
       break;
-    case "PlusNode": {
-      interpret(node.arg1, (val1) => {
-        interpret(node.arg2, (val2) => {
-          const results = [];
-          const values1 = Array.isArray(val1) ? val1 : [val1];
-          const values2 = Array.isArray(val2) ? val2 : [val2];
-          for (const v1 of values1) {
-            for (const v2 of values2) {
-              results.push(v1 + v2);
-            }
-          }
-          cont(results);
-        });
-      });
+    case "PlusNode":
+      interpretBinaryOp(node, cont, (a, b) => a + b);
       break;
-    }
-    case "TimesNode": {
-      interpret(node.arg1, (val1) => {
-        interpret(node.arg2, (val2) => {
-          const results = [];
-          const values1 = Array.isArray(val1) ? val1 : [val1];
-          const values2 = Array.isArray(val2) ? val2 : [val2];
-          for (const v1 of values1) {
-            for (const v2 of values2) {
-              results.push(v1 * v2);
-            }
-          }
-          cont(results);
-        });
-      });
+    case "TimesNode":
+      interpretBinaryOp(node, cont, (a, b) => a * b);
       break;
-    }
-    case "AmbNode": {
-      const collectResults = (results) => (value) => {
-        if (Array.isArray(value)) {
-          results.push(...value);
-        } else {
-          results.push(value);
-        }
-      };
-      const results = [];
+    case "MinusNode":
+      interpretBinaryOp(node, cont, (a, b) => a - b);
+      break;
+    case "DivideNode":
+      interpretBinaryOp(node, cont, (a, b) => a / b);
+      break;
+    // Run the continuation for each value in the AmbNode.
+    case "AmbNode":
       for (const value of node.values) {
-        interpret(value, collectResults(results));
+        interpret(value, cont);
       }
-      cont(results);
       break;
-    }
     default:
       throw new Error(`Unknown node type: ${node.type}`);
   }
 }
 
+// The outermost continuation just collects up all results
+// from the sub-paths of execution
 function evaluateAST(ast) {
   const results = [];
   interpret(ast, (value) => {
-    if (Array.isArray(value)) {
-      results.push(...value);
-    } else {
-      results.push(value);
-    }
+    results.push(value);
   });
   return results;
 }
@@ -72,14 +49,14 @@ const program1 = {
   type: "PlusNode",
   arg1: {
     type: "TimesNode",
-    arg1: { type: "NumberNode", value: 2 },
+    arg1: { type: "NumberNode", value: 1 },
     arg2: { type: "NumberNode", value: 3 },
   },
   arg2: { type: "NumberNode", value: 5 },
 };
 
 const program2 = {
-  type: "PlusNode",
+  type: "TimesNode",
   arg1: {
     type: "AmbNode",
     values: [
@@ -96,5 +73,34 @@ const program2 = {
   },
 };
 
-// console.log("Program 1:", evaluateAST(program1));
+const program3 = {
+  type: "PlusNode",
+  arg1: {
+    type: "TimesNode",
+    arg1: {
+      type: "AmbNode",
+      values: [
+        { type: "NumberNode", value: 1 },
+        { type: "NumberNode", value: 2 },
+      ],
+    },
+    arg2: {
+      type: "AmbNode",
+      values: [
+        { type: "NumberNode", value: 3 },
+        { type: "NumberNode", value: 4 },
+      ],
+    },
+  },
+  arg2: {
+    type: "AmbNode",
+    values: [
+      { type: "NumberNode", value: 5 },
+      { type: "NumberNode", value: 6 },
+    ],
+  },
+};
+
+console.log("Program 1:", evaluateAST(program1));
 console.log("Program 2:", evaluateAST(program2));
+console.log("Program 3:", evaluateAST(program3));
